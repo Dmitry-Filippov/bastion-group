121751a05bf07e5ed904c79d7ba3d8d98a2b1e9a
Dmitry Filippov <demodima97@mail.ru>
2022-07-19 03:13:55 +0300

    footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      {/* <form className="footer__form">
+        <input type="text" className="footer__input" placeholder="Ваше имя" />
+        <input type="phone" className="footer__input" placeholder="Ваш телефон" />
+        <button className="footer__submit-btn" type="submit" id="sub">Заказать звонок</button>
+        <label htmlFor="sub">Нажимая на кнопку</label>
+      </form> */}
+      
+    </footer>
+  );
+}
+
+export default Footer;
